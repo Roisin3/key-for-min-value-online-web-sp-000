aa097071166de045e7ad5959826a94a8e049dbfe
Michael Lynch <michael@michaelrlynch.com>
2019-08-20 05:00:10 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,7 +6,7 @@ def key_for_min_value(name_hash)
   low_val = nil
   name_hash.each do |key, val|
     if low_val == nil || val < low_val
-      low_val = v
+      low_val = val
       low_key = key
     end
   end
